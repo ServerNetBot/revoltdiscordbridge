ee11fc1a7cbb3a34b16962d784207965418ad4c5
Oreo <thediamondgamer331@gmail.com>
2022-01-29 21:05:07 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const mod = require('revcjatapi');
+const revolt = ""; // Revolt Bot Token
+const discord = ""; // Discord Bot Token
+const revChannels = []; // Revolt channels
+const discordChannels = []; // Discord Channels
+const Discord = require('discord.js');
+
+const Client = new mod.Client(revolt, true);
+const client = new Discord.Client({
+    intents: ["GUILDS", "GUILD_MESSAGES"]
+});
+
+Client.on('ready', () => {
+    console.log(`Now logged in as ${Client.username}`);
+})
+
+Client.on('message', (msg) => {
+    if (!revChannels.includes(msg.channel.id)) return;
+    if (msg.author.isBot) return;
+
+    let rcnt = `[REVOLT] **@${msg.author.username}:** ${msg.content}`;
+    let dcnt = `[REVOLT] **@${msg.author.username}:** ${msg.content}`;
+
+    for (const channelID of revChannels) {
+        Client.sendToChannel(channelID, { content: rcnt });
+    }
+    for (const channelID of discordChannels) {
+        const channel = client.channels.cache.get(channelID) || client.channels.fetch(channelID).catch((e) => { console.log(e); return });
+
+        channel.send({ content: dcnt });
+    }
+})
+
+client.login(discord)
